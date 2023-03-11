b1d1d9cf618417c6d1d807e2608e958815b9f1f6
aashish2604 <aashish.260401@gmail.com>
2023-03-11 20:40:53 +0530

    feat: image storage and create preview

---
+++ lib/services/image_processing.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:path_provider/path_provider.dart';
+
+class ImageProcessing {
+  Future<void> storeImage(File file) async {
+    try {
+      final fileName = file.path.split('/').last;
+      var directory = await getApplicationDocumentsDirectory();
+      final File destinationFile = File('${directory.path}/$fileName');
+      await file.copy(destinationFile.path);
+      Fluttertoast.showToast(msg: 'Image saved successfully');
+    } on Exception catch (e) {
+      print(e.toString());
+      Fluttertoast.showToast(msg: 'Error occured in saving the image');
+    }
+  }
+
+  Future<Image?> getPreviewImage() async {
+    try {
+      Directory directory = await getApplicationDocumentsDirectory();
+      final fetchedFile = await directory.list().last;
+      File file = File(fetchedFile.path);
+      Image image = Image.file(
+        file,
+        fit: BoxFit.fill,
+      );
+      return image;
+    } on Exception catch (e) {
+      Fluttertoast.showToast(msg: "Nothing found");
+      return null;
+    }
+  }
+}
